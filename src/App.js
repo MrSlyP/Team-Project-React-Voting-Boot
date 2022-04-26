8668c85a2347b9644ce039ceea94131be222c8c1
MrSlyP <99894989+MrSlyP@users.noreply.github.com>
2022-04-26 15:20:20 -0400

    Création du App.js initial et autres fichiers react dans la branch sly

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+     
+    
+    Mettre components ici
+    
+      </header>
+    </div>
+  );
+}
+
+export default App;
