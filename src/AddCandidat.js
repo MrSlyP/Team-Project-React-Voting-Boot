e3692e96fd8747fd806933f46e38cd1d04469f33
klacsamana15 <klacsamana15@gmail.com>
2022-04-29 23:34:18 -0400

    Console log test success: entered name displaying correctly

--- src/AddCandidat.js
+++ src/AddCandidat.js
@@ -1,31 +1,53 @@
-import React from "react";
+import React, {useState} from "react";
 
 function AddCandidat(props) {
   //Idée initiale: que l'ajout d'un candidat l'ajoute
   //à candidateList et modifie l'index du state
-  let changeIndex = props.changeIndex;
-  let index = props.indexValue;
+  //let changeIndex = props.changeIndex;
+  //let index = props.indexValue;
+
+  const[enteredName, setEnteredName] = useState("");
+
+  const NameHandler = (event) => {
+    //function to retrive candidate name entered
+    setEnteredName(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    //prevents form from reloading after clicking button
+    event.preventDefault();
+    //candidate name entered is being stored in variable
+    const candidateNames = {enteredName};
+    console.log(candidateNames);
+
+    //reset textbox
+    setEnteredName("");
+
+ 
+
+};
+
   return (
-    <div>
-      Ici on gère l'ajout
+    <form onSubmit={submitHandler}>
       <div className="form-group">
         <label htmlFor="candidateInput"></label>
         <input
-          type="nameCandidate"
+          type="text"
+          //type="nameCandidate"
           className="form-control"
-          id="nameCandidate"
+          //id="nameCandidate"
           placeholder="Candidate's name"
+          value={enteredName}
+          onChange={NameHandler}
         />
-
-        <button onClick={(e) => {
-          changeIndex(e);
-        }}
+        <button
         type="submit" className="btn btn-primary">
-          Add candidate no {index + 1} 
+          Add{/* Add candidate no {index + 1}  */}
         </button>
       </div>
-    </div>
+    </form>
   );
+
 }
 
 export default AddCandidat;
