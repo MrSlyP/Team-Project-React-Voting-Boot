c5353b6470bd4dde9ee5ef1428dec9374f390475
klacsamana15 <klacsamana15@gmail.com>
2022-04-29 23:44:23 -0400

    Console log test success: props retrieved from AddCandidat.js

--- src/ListCandidat.js
+++ src/ListCandidat.js
@@ -1,14 +1,24 @@
 import React, { useState } from "react";
+import AddCandidat from "./AddCandidat";
 
 function ListCandidat(props) {
     //But: afficher les candidats à mesure qu'ils
     //sont ajoutés.
-    let liste = props.liste
+    //let liste = props.liste
+
+    //handling values passed as props
+    //retrieving candidateNames
+    const onSaveHandler = (enteredCandidate) => {
+      const candidates = {enteredCandidate};
+      console.log(candidates);
+    };
 
     return (
       <div>
-          Affichage Candidat {" "}
-          {liste}
+          {/* Affichage Candidat {" "}
+          {liste} */}
+          {/* retrieving candidateNames value from AddCandidat.js */}
+          <AddCandidat onSaveCandidat={onSaveHandler}/>
       </div>
     )
   }
