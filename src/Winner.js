d76c17a6aa7e7265355722a501eb9563efdacb3f
klacsamana15 <klacsamana15@gmail.com>
2022-05-03 12:09:31 -0400

    affiche le nom du gagnant

--- src/Winner.js
+++ src/Winner.js
@@ -1,13 +1,26 @@
 import React from "react";
 
 function Winner(props) {
-    //But: afficher le candidat gagnants ET
-    //tous les candidats avec leur nombre de votes.
+    //retrieving data passed from App.js
+    //storing the candidateList value from App.js in new variable finalList
+    let finalList = props.result;
+    
+    //mapping array to find the highest votes
+    const highestVotes = Math.max(...finalList.map(candidate => candidate.nbVote ));
+    
+    //finding candidate that matches highest votes
+    const winner = finalList.find(candidate => candidate.nbVote === highestVotes);
+   
     return (
-      <div>
-          Affichage du gagnant
+        <div>
+            <ul className="list-group mb-3">
+            <li className="list-group-item list-group-item-light">
+                <h1>Winner</h1>
+                <p>{winner.name}</p>
+            </li>
+            </ul>
       </div>
-    )
+    );
   }
   
   export default Winner;
