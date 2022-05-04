fd87a76a74aba9f68bd2e2a60f27be05a1a45dce
MrSlyP <99894989+MrSlyP@users.noreply.github.com>
2022-05-04 17:25:10 -0400

    egality show in the console and minor changes to ListCandidat

--- src/ListCandidat.js
+++ src/ListCandidat.js
@@ -1,16 +1,11 @@
-import React, { useState } from "react";
-
-
 function ListCandidat(props) {
-    //But: afficher les candidats à mesure qu'ils sont ajoutés.
-
-    //retrieving data passed from App.js
-    //storing the candidateList value from App.js in new variable candidateList
-    const candidateList = props.liste
-
+    //But: afficher les candidats à mesure qu'ils
+    //sont ajoutés.
+    const listeC = props.liste
+    
     return (
       <ul className="list-group mb-3">
-         {candidateList.map(({ name, nbVote }) => (
+         {listeC.map(({ name, nbVote }) => (
         <li className="list-group-item list-group-item-light" 
         key={name}>{name} 
         </li>
--- src/Winner.js
+++ src/Winner.js
@@ -10,15 +10,38 @@ function Winner(props) {
     
     //finding candidate that matches highest votes
     const winner = finalList.find(candidate => candidate.nbVote === highestVotes);
-   
+    
+    //A variable to stock the value of the numbers of vote of the winner
+    let val = winner.nbVote
+
+    //function to retreive all candidate's name who has same number of vote (num here)
+    //as the winner, in case of equality
+    function getAllIndexes(arr, num) {
+      let indexes = []
+      
+      for (let i =0 ; i<arr.length; i++){
+          if(arr[i].nbVote===num)
+          indexes.push(arr[i].name);
+      }
+      return indexes;
+  }
+    
+     
+     //delare a array to stock all the name of the winners (witch you get by the function getAllIndexes)
+     let winnerList = getAllIndexes(finalList, val);
+     
+     //to check, it does write into the console
+     console.log(winnerList)
+
     return (
         <div>
-            <ul className="list-group mb-3">
-            <li className="list-group-item list-group-item-light">
-                <h1>Winner</h1>
-                <p>{winner.name}</p>
-            </li>
-            </ul>
+          WINNER
+        <ul className="list-group mb-3">
+          
+         {winnerList.map((nom,index)=> {<p key={index}>{nom}</p>})}
+        
+      
+        </ul>
       </div>
     );
   }
