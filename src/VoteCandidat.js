5d4c2ac61044b2c77c0bfa11c73a2217e0f008c2
klacsamana15 <klacsamana15@gmail.com>
2022-05-04 11:17:29 -0400

    bouton end vote et components transférés

--- src/VoteCandidat.js
+++ src/VoteCandidat.js
@@ -1,33 +1,64 @@
+import React, { useState } from "react";
+import Winner from "./Winner";
+import VotesTotal from "./VotesTotal";
 
 function VoteCandidat(props) {
-
-   //retrieving function passed from App.js
+  
+  //retrieving function passed from App.js
   //function to update the number of votes in the state
-  let incrementCount = props.incrementNbVote
+  let incrementCount = props.incrementNbVote;
 
   //retrieving data passed from App.js
   //storing the candidateList value from App.js in new variable voteNames
-  let voteNames = props.voteList
- 
-    return (
-        <div>
-          {/* mapping through candidateList array 
+  let voteNames = props.voteList;
+
+  //Declaring boolean for 'end vote' button
+  //initialized at false
+  const [endVoting, setEndVoting] = useState(false);
+
+  //function endVoteHandler handles button 'End Vote!'
+  //at first, it is set to !endVoting : not end Voting
+  //on click, it is set to endVoting : voting stopped
+  function endVoteHandler() {
+    setEndVoting(!endVoting);
+  }
+
+  return (
+    <div>
+      {/* if !endVoting, display button 'End Vote' */}
+      {!endVoting && (
+        <button className="btn btn-danger mb-3" 
+        onClick={endVoteHandler}>End Vote!</button>)}
+      {/* mapping through candidateList array 
           deconstructing the object to retrieve name and nbVote
           returning i: index of selected vote button
           index is then passed in function incrementCount
-          which will update number of votes  */}
-        <ul className="list-group mb-3 ">
-         {voteNames.map(({name, nbVote},i) => (
-        <li className="list-group-item list-group-item-light" 
-        key={name}>{name} Votes: {nbVote}
-        <button type="button" 
-        className="btn btn-primary ml-3"
-        onClick={() => incrementCount(i)}>Vote</button>
-        </li> 
-      ))}
-      </ul>
-        </div>
-    );
-  }
-  
-  export default VoteCandidat;
+          which will update number of votes
+          if !endVoting, display list of candidates to vote and button vote   */}
+      {!endVoting && 
+        (<ul className="list-group mb-3 ">
+          {voteNames.map(({ name, nbVote }, i) => (
+            <li className="list-group-item list-group-item-light" 
+            key={name}>
+              {name} Votes: {nbVote}
+              <button
+                type="button"
+                className="btn btn-primary ml-3"
+                onClick={() => incrementCount(i)}>Vote</button>
+            </li>
+          ))}
+        </ul>
+      )}
+
+      {/* if endVoting, show Winner and VotesTotal component, else null */}
+      <div>
+      {endVoting ? <Winner result={voteNames} /> : null}
+      </div>
+      <div>
+      {endVoting ? <VotesTotal total={voteNames} /> : null}
+      </div>
+    </div>
+  );
+}
+
+export default VoteCandidat;
